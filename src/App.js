080a8091864231cac136c444ead934d13c5404e3
yasir ahmad <khan10055yasir@gmail.com>
2022-06-01 21:37:56 +0500

    initial Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { createContext } from "react";
+import "./style.css"
+import TableView from "./TableView";
+
+function App(){
+
+  
+  return(
+    <>
+   
+   <TableView/>
+   
+    </>
+  )
+}
+
+export default App;
