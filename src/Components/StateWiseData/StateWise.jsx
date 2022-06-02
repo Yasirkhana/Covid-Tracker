6baa1b036cd2eedb7a70c2bd682f6b67e8a22178
yasir ahmad <khan10055yasir@gmail.com>
2022-06-02 20:28:18 +0500

    Update StateWise.jsx

--- src/Components/StateWiseData/StateWise.jsx
+++ src/Components/StateWiseData/StateWise.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./SateWise.css"
 
 export const StateWise = () => {
     
+    
+
+    const getcovidData= async() =>{
+        const res =  await fetch('https://covid19india.org/data.json')
+        const actualData = await res.json()
+        console.log(actualData.statewise);
+    }
+
+
     useEffect(() => {
       getcovidData();
         }, [])
