d1eb9aa92f2001270393520c4cee5753c7f627c3
yasir ahmad <khan10055yasir@gmail.com>
2022-06-03 00:16:22 +0500

    initai

---
+++ src/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div><h1>THIS IS ABOUT PAGE</h1></div>
+  )
+}
+
+export default About
--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React, { createContext } from "react";
-import "./style.css"
-import { StateWise } from "./Components/StateWiseData/StateWise";
+import React from 'react'
+import { BrowserRouter as Router, Routes , Route } from 'react-router-dom'
+import Home from './Home'
+import Service from './Service'
+import Contact from './Contact'
+import About from './About'
 
-function App(){
+import { Link } from 'react-router-dom'
 
-  
-  return(
-    <>
-   
-   <StateWise></StateWise>
-   
-    </>
+
+function App() {
+  return (
+    <div>
+      <Router>
+              
+        <nav>
+            <Link To="/">HOME</Link>
+            <Link To="/about">ABOUT</Link>
+            <Link To="/service">SERVICE</Link>
+            <Link To="/contact">CONTACT</Link>
+        </nav>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/service' element={<Service/>} />
+          <Route path='/contact' element={<Contact/>} />
+          <Route path='/about' element={<About/>} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
-export default App;
+export default App
